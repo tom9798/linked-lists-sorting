cbacdce89a146857a1bb8c096bf50591e8e3e3cd
tom9798 <soustieltom@gmail.com>
2023-12-02 15:46:07 +0200

    better now, smarter maxExp finding

--- radixSortLinkedList.cpp
+++ radixSortLinkedList.cpp
@@ -2,6 +2,16 @@
 #include <math.h>
 using namespace std;
 
+LinkedList* generateRandomLinkedList(int size){ //generates random linked list
+    LinkedList *arr = new LinkedList();
+    int i;
+    for (i = 0; i < size; i++) {
+        arr->pushToEnd(&arr->head, rand() % 10000);
+    }
+    return arr;
+}
+
+//I know it looks bad, but it's pretty cheap comparing to other options
 Node* zero = NULL;
 Node* one = NULL;
 Node* two = NULL;
@@ -24,30 +34,13 @@ Node* initial7 = NULL;
 Node* initial8 = NULL;
 Node* initial9 = NULL;
 
-int digit(int num, int exp){
+int digit(int num, int exp){ //returns digit of a number by its exponent
     int toDevide = pow(10, exp);
     int digit = (num/toDevide) % 10;
     return digit;
 }
 
-int maxExp(Node** head){
-    int max = (*head)->Value;
-    Node* temp = *head;
-    while (temp->Next!=NULL){
-        if (temp->Value>max){
-            max = temp->Value;
-        }
-        temp = temp->Next;
-    }
-    int exp = 0;
-    while (max>0){
-        max = max/10;
-        exp++;
-    }
-    return exp;
-}
-
-void reset(){
+void reset(){ //resets all the pointers
     zero = NULL;
     one = NULL;
     two = NULL;
@@ -71,14 +64,14 @@ void reset(){
     initial9 = NULL;
 }
 
-void distribute(Node* temp, int digiti){
+void distribute(Node* temp, int digiti){ //distributes nodes by their digit
     switch (digiti) {
         case 0:
-            if (zero == NULL) {
+            if (zero == NULL) { //if the list is empty - initialize it with the new node and save the first node, so we can connect it later
                 zero = temp;
                 initial0 = zero;
                 break;
-            } else {
+            } else { //if the list is not empty - connect the last node to the new node, and save the new node as the last node
                 zero->Next = temp;
                 zero = zero->Next;
                 break;
@@ -185,24 +178,42 @@ void distribute(Node* temp, int digiti){
     }
 }
 
-Node* countingSort(Node** head, int exp, LinkedList* newList) {
+int countExp = 0; //static variables for the counting sort, in bigger lists it saves a lot of iterations
+int maxNum = 0;
+
+Node* countingSort(Node** head, int exp, LinkedList* newList) { //counting sort
     reset();
     Node *temp = *head;
     newList->head = *head;
     while (temp->Next != NULL) { //counting
-        int digiti = digit(temp->Value, exp);
+        if (countExp == 0 && temp->Value > maxNum) { //getting the max number
+            maxNum = temp->Value;
+        }
+        int digiti = digit(temp->Value, exp); // sorry for the bad variable names
         distribute(temp, digiti);
         temp = temp->Next;
     }
-    int digiti = digit(temp->Value, exp);
+    if (countExp == 0) { //getting the max exponent
+        if (temp->Value > maxNum) {
+            maxNum = temp->Value;
+        }
+        while (maxNum > 0) { //that'll happen only once
+            maxNum = maxNum / 10;
+            countExp++;
+        }
+    }
+    int digiti = digit(temp->Value, exp); //distributing the last node
     distribute(temp, digiti);
 
-    Node* ends[10] = {zero, one, two, three, four, five, six, seven, eight, nine};
-    Node* starts[10] = {initial0, initial1, initial2, initial3, initial4, initial5, initial6, initial7, initial8, initial9};
+    Node* ends[10] = {zero, one, two, three, four, five, six, seven, eight, nine}; //array of the last nodes of each list
+    Node* starts[10] = {initial0, initial1, initial2, initial3, initial4, initial5, initial6, initial7, initial8, initial9}; //array of the first nodes of each list
+    // I could've used a linked list, but it would take longer. I know that each list will be 10 nodes because it's only for each digit, which is not that expansive memory-wise when comparing to big lists,
+    //and a lot quicker when accessing the nodes, because I don't have to iterate through the list to get to the last node, I can just access it by its index (happens each iteration).
+    //plus it doesn't need to be created each iteration, it's always the same order, the nodes contain the data.
 
     bool first = true;
     int i, j;
-    for (i = 0; i < 9; i++) {
+    for (i = 0; i < 9; i++) { //connecting the lists
         if (starts[i] != NULL) {
             if (first) {
                 newList->head = starts[i];
@@ -217,36 +228,34 @@ Node* countingSort(Node** head, int exp, LinkedList* newList) {
             }
         }
     }
+    newList->printList(newList->head); //printing the list after each iteration to see the progress (optional)
+    printf("\n");
     return newList->head;
 }
 
 Node* driver (Node** head){
     LinkedList *tempo = new LinkedList();
-    int maxexp = maxExp(head);
     int exp = 0;
     Node* temp = *head;
-    while (exp<maxexp){
+    temp = countingSort(&temp, exp, tempo); //first iteration, getting the max exponent
+    exp++;
+    while (exp<countExp){ //sorting by each exponent
         temp = countingSort(&temp, exp, tempo);
-        tempo->printList(tempo->head);
-        printf("\n");
         exp++;
     }
+    countExp = 0; //resetting the static variables, so the function can be called again
+    maxNum = 0;
     return tempo->head;
 }
 
 int main() {
-//    setting my array to sort
-    LinkedList *arr = new LinkedList();
-    int toAdd[] = {195,469, 953,100, 112, 1654, 0, 2, 89, 16, 17, 3, 1, 4, 3000, 32, 5324, 7123, 215, 56, 9, 723, 744, 854, 823, 1, 1, 587, 1234, 876, 546, 19, 87, 69};
-    int size = sizeof(toAdd) / sizeof(toAdd[0]);
-    int i;
-    for (i = 0; i < size; i++) {
-        arr->pushToEnd(&arr->head, toAdd[i]);
-    }
-    printf("Original array: \n");
+    printf("start generating random linked list: \n");
+    LinkedList* arr = generateRandomLinkedList(100);
     arr->printList(arr->head);
-
     printf("\n\nstart sorting: \n");
     driver(&arr->head);
+
+    printf("\nfinished sorting: \n");
+    arr->printList(arr->head);
     return 0;
 }
